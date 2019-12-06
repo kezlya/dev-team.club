188d05f282fb8902a57ea23172b314607cf5d672
Klimonov <22pov22@gmail.com>
2019-12-06 19:52:40 +0300

    project section

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="sections-wrapper">
 		<team></team>
+		<projects></projects>
 	</div>
 </template>
 
 <script>
 import Team from '~/components/Team.vue'
+import Projects from '~/components/Projects.vue'
 
 
 export default {
 	components: {
 		Team,
+		Projects,
 	}
 }
 </script>
